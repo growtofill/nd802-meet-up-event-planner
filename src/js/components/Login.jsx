c4df3de2e3b19c374b729e98aa009c56103bcdbb
Pavlo Mykhalov <9610200@gmail.com>
2016-02-22 19:47:12 +0200

    Add onBlur validation for the login form

--- src/js/components/Login.jsx
+++ src/js/components/Login.jsx
@@ -2,9 +2,9 @@ import React, { Component } from 'react';
 
 const minPasswordLength = 8;
 const passwordRequirements = [
-    /[a-z]/,
+    /\d/,
     /[A-Z]/,
-    /\d/
+    /[a-z]/
 ];
 
 export default class Login extends Component {
@@ -13,7 +13,9 @@ export default class Login extends Component {
 
         this.state = {
             passwordLength: 0,
-            isRequirementsMatch: false
+            nameValidationMessage: '',
+            emailValidationMessage: '',
+            passwordValidationMessage: ''
         };
     }
     componentDidMount() {
@@ -22,27 +24,56 @@ export default class Login extends Component {
     render() {
         let {
             passwordLength,
-            isRequirementsMatch
+            nameValidationMessage,
+            emailValidationMessage,
+            passwordValidationMessage
         } = this.state;
 
         return (
             <form className="Login" action="." method="POST" onSubmit={e => this.onSubmit(e)} ref="root">
                 <label className="Login-field">
                     <span className="Login-field-label">Name</span>
-                    <input type="text" className="Login-field-input" ref="name" autoComplete="name" required/>
+                    <input
+                        type="text"
+                        name="name"
+                        className={`Login-field-input ${nameValidationMessage ? ' Login-field-input--invalid' : ''}`}
+                        ref="name"
+                        autoComplete="name"
+                        onFocus={e => this.resetValidationMessage(e)}
+                        onBlur={e => this.setValidationMessage(e)}
+                        required
+                    />
+                    {nameValidationMessage
+                        ? <span className="Login-field-validationHint">{nameValidationMessage}</span>
+                        : null}
                 </label>
                 <label className="Login-field">
                     <span className="Login-field-label">Email</span>
-                    <input type="email" className="Login-field-input" ref="email" autoComplete="email" required/>
+                    <input
+                        type="email"
+                        name="email"
+                        className={`Login-field-input ${emailValidationMessage ? ' Login-field-input--invalid' : ''}`}
+                        ref="email"
+                        autoComplete="email"
+                        onFocus={e => this.resetValidationMessage(e)}
+                        onBlur={e => this.setValidationMessage(e)}
+                        required
+                    />
+                    {emailValidationMessage
+                        ? <span className="Login-field-validationHint">{emailValidationMessage}</span>
+                        : null}
                 </label>
                 <label className="Login-field">
                     <span className="Login-field-label">Password</span>
                     <div className="Login-field-container">
                         <input
                             type="password"
-                            className="Login-field-input"
+                            name="password"
+                            className={`Login-field-input ${passwordValidationMessage ? ' Login-field-input--invalid' : ''}`}
                             ref="password"
                             autoComplete="new-password"
+                            onFocus={e => this.resetValidationMessage(e)}
+                            onBlur={e => this.setValidationMessage(e)}
                             onChange={e => this.onPasswordChange(e)}
                             required
                         />
@@ -51,7 +82,14 @@ export default class Login extends Component {
                             max={minPasswordLength}
                             value={passwordLength}
                         />
+                        <small>
+                            Use a digit, an upper-case and a lower-case letter. At least 8
+                            characters long.
+                        </small>
                     </div>
+                    {passwordValidationMessage
+                        ? <span className="Login-field-validationHint">{passwordValidationMessage}</span>
+                        : null}
                 </label>
                 <button type="submit" className="Login-button" ref="submitButton">Register</button>
             </form>
@@ -63,12 +101,28 @@ export default class Login extends Component {
         e.preventDefault();
     }
     onPasswordChange(e) {
+        let element = e.target;
         let password = e.target.value;
         let isRequirementsMatch = passwordRequirements.every(r => password.match(r));
+        let validity = isRequirementsMatch ? '' : 'Please match the requested format.';
+
+        element.setCustomValidity(validity);
 
         this.setState({
-            passwordLength: password.length,
-            isRequirementsMatch
+            passwordLength: password.length
         });
+    }
+    setValidationMessage(e) {
+        let { name, validationMessage } = e.target;
+
+        this.setState({
+            [name + 'ValidationMessage']: validationMessage
+        });
+    }
+    resetValidationMessage(e) {
+        let { name } = e.target;
+        this.setState({
+            [name + 'ValidationMessage']: ''
+        })
     }
 }
